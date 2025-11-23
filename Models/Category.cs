889b267289d1e7b3e0ce43766aecdb9788f5a64d
Nihat Aydın <nihatydin1@gmail.com>
2025-11-24 00:58:33 +0300

    new migration

--- Models/Category.cs
+++ Models/Category.cs
@@ -5,5 +5,5 @@ public class Category
     public int Id { get; set; }
     public string CategoryName { get; set; } = null!;
     public string Url { get; set; } = null!;
-    public List<Product>? Products { get; set; } = new ();
+    public List<Product> Products { get; set; } = new ();
 }
